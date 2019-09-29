b6f23336d0f80f9054896951ca2d9860dfada9e4
Zachary Schulz <zjschulz@gmail.com>
2019-09-29 14:30:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+# Make your shoe class here!class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
